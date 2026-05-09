a621cee82a62847e45e551b90f0fb390067b056a
kelvin-melfah <kmelfah@byupathway.edu>
2026-05-09 15:39:51 +0000

    Changes 1

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!!!");
+        Console.WriteLine("Hello World!!");
         Console.WriteLine("This is in C#");
 
         Console.WriteLine("What is your favorite color? ");
